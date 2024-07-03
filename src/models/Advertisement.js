c815956b84301b06ef3c5be8c86287f7bae2b5a5
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-07-02 21:11:48 -0300

    :recycle:refactor: refatora o schema do Advertisement(anúncio).

--- src/models/Advertisement.js
+++ src/models/Advertisement.js
@@ -1,21 +1,17 @@
 import mongoose from "mongoose";
 
 const advertisementSchema = new mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId },
-    idUser: { type: String, required: true },
-    nameUser: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     brand: { type: String, required: true },
     model: { type: String, required: true },
     year: { type: String, required: true },
     color: { type: String, required: true },
     km: { type: String, required: true },
     image: { type: String, required: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    createdAt: { type: String, required: true }
 }, { versionKey: false });
 
-const advertisement = mongoose.model("advertisements", advertisementSchema);
+const Advertisement = mongoose.model("Advertisement", advertisementSchema, "advertisements");
 
-export default advertisement
+export default Advertisement
