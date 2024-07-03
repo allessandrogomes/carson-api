3cc85fc30344c06ef95f7864c74e4e54086db913
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-07-02 21:13:20 -0300

    :sparkles:feat: adiciona a rota de registro de um novo usuário.

--- src/routes/advertisementsRoutes.js
+++ src/routes/advertisementsRoutes.js
@@ -3,18 +3,21 @@ import AdvertisementController from "../controllers/advertisementController.js"
 
 const routes = express.Router();
 
-//Lista todos os anúncios
+// Lista todos os anúncios
 routes.get("/advertisements", AdvertisementController.listAdvertisements);
 
 // Filtra e lista os anúncios pela busca(marca, modelo e ano), estado, cor, marca, ano e preço
 routes.get("/advertisements/search", AdvertisementController.searchAdvertisements);
 
+// Registra um novo anúncio
+routes.post("/advertisement/register", AdvertisementController.registerAdvertisement);
+
 // Retorna o anúncio pelo _id
-routes.get("/advertisement", AdvertisementController.listAdvertisementById);
+// routes.get("/advertisement", AdvertisementController.listAdvertisementById);
 
 
-routes.post("/advertisements", AdvertisementController.registerAdvertisement);
-routes.put("/advertisements/:id", AdvertisementController.updateAdvertisement);
-routes.delete("/advertisements/:id", AdvertisementController.deleteAdvertisement);
+
+// routes.put("/advertisements/:id", AdvertisementController.updateAdvertisement);
+// routes.delete("/advertisements/:id", AdvertisementController.deleteAdvertisement);
 
 export default routes;
