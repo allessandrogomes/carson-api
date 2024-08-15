f15193047c2bbda801c501a747cf839e631dc1ee
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-08-15 15:13:47 -0300

    :card_file_box:raw: altera configurações de conexão.

--- server.ts
+++ server.ts
@@ -1,15 +1,8 @@
 import 'dotenv/config'
 import app from './src/app.js'
-import https from 'https'
-import fs from 'fs'
-
-const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert'),
-}
 
 const PORT = process.env.PORT || 3001
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log('API rodando na porta https://localhost:3001')
+app.listen(PORT, () => {
+  console.log(`API rodando na porta http://localhost:${PORT}`)
 })
--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,30 @@ import express from 'express'
 import connectToDatabase from './config/dbConnect'
 import routes from './routes/index'
 
-const connectionAPI = await connectToDatabase()
+const app = express()
 
-if (connectionAPI) {
-  connectionAPI.on('error', (error: Error) => {
-    console.error('Erro de conexão', error)
-  })
+try {
+  const connectionAPI = await connectToDatabase()
 
-  connectionAPI.once('open', () => {
-    console.log('Conexão bem sucedida.')
-  })
+  if (connectionAPI) {
+    connectionAPI.on('error', (error: Error) => {
+      console.error('Erro de conexão', error)
+    })
+
+    connectionAPI.once('open', () => {
+      console.log('Conexão bem sucedida.')
+    })
+  } else {
+    console.error(
+      'Falha na conexão com o banco de dados. Verifique as configurações e tente novamente.',
+    )
+    process.exit(1) // Encerra o processo se a conexão falhar
+  }
+} catch (error) {
+  console.error('Erro ao conectar ao banco de dados:', error)
+  process.exit(1) // Encerra o processo se ocorrer um erro
 }
 
-const app = express()
 routes(app)
 
 export default app
