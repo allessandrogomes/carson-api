c6a96052a295e8db81ff494fe404a424f6cf2c8b
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-08-19 11:30:34 -0300

    :wrench:build: adiciona .js nos imports de app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import connectToDatabase from './config/dbConnect'
-import routes from './routes/index'
+import connectToDatabase from './config/dbConnect.js'
+import routes from './routes/index.js'
 
 const app = express()
 
